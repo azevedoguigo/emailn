a99875f24993b111be96c50900a2341245bab220
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2025-01-28 20:12:06 -0300

    fix: build error.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -18,11 +18,11 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	campaignService := campaign.Service{
+	campaignService := campaign.ServiceImp{
 		Repository: &database.CampaignRepository{},
 	}
 	handler := endpoint.Handler{
-		CampaignService: campaignService,
+		CampaignService: &campaignService,
 	}
 
 	r.Post("/campaign", endpoint.HandlerError(handler.CampaignPost))
